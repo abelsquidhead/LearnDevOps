1fa87955b801a0807b439d39c6bd29376c729089
abel <abel.wang@microsoft.com>
2019-04-08 20:31:02 -0700

    updating arm templates and helm charts

--- Application/server.js
+++ Application/server.js
@@ -1,88 +1,15 @@
-#!/usr/bin/env node
-
-/**
- * Module dependencies.
- */
-
-var app = require('./app');
-var debug = require('debug')('NodeAppForTripleCrown:server');
+'use strict';
 var http = require('http');
+var port = process.env.PORT || 8080;
+var fs = require('fs');
+const appInsights = require('applicationinsights');
+appInsights.setup();
+appInsights.start();
 
-/**
- * Get port from environment and store in Express.
- */
-
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-   var port = parseInt(val, 10);
-
-   if (isNaN(port)) {
-      // named pipe
-      return val;
-   }
-
-   if (port >= 0) {
-      // port number
-      return port;
-   }
-
-   return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-   if (error.syscall !== 'listen') {
-      throw error;
-   }
-
-   var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
-
-   // handle specific listen errors with friendly messages
-   switch (error.code) {
-      case 'EACCES':
-         console.error(bind + ' requires elevated privileges');
-         process.exit(1);
-         break;
-      case 'EADDRINUSE':
-         console.error(bind + ' is already in use');
-         process.exit(1);
-         break;
-      default:
-         throw error;
-   }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-   var addr = server.address();
-   var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-   debug('Listening on ' + bind);
-}
-
-module.exports = server;
+http.createServer(function (req, res) {
+    fs.readFile('index.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
+        res.write(data);
+        res.end();
+    });
+}).listen(port);
